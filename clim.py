93f5d0c59d575c8f54f17000a7cc7fcae24db783
Johnny Lighthouse <git.x.jjl@9ox.net>
2011-02-22 22:42:56 -0500

    flesh out parameter change detection and logging

--- clim.py
+++ clim.py
@@ -5,11 +5,17 @@ import math
 import ConfigParser
 import logging
 
-LEVELS = {'debug': logging.DEBUG,
-          'info': logging.INFO,
-          'warning': logging.WARNING,
-          'error': logging.ERROR,
+LEVELS = {'debug'   : logging.DEBUG,
+          'info'    : logging.INFO,
+          'warning' : logging.WARNING,
+          'error'   : logging.ERROR,
           'critical': logging.CRITICAL}
+
+MajorPeriod = 1
+MinorPeriod = .5
+LED = 6004
+Log_Filename = "incline.log"
+Log_Level = "info"
 
 #Initilize Labjack
 import u3
@@ -21,8 +27,6 @@ State = ()
 #set an initial Target time for first iteration and round up to a whole second
 Target = math.ceil(time.time())
 
-MajorPeriod=()
-
 while 1 != 0:
 
 	Start = time.time()
@@ -30,34 +34,53 @@ while 1 != 0:
 	#
 	#read config file and set default values.  default values don't work as intended.
 	#
-	config = ConfigParser.RawConfigParser({'MajorPeriod': '1', 'MinorPeriod': '.5'})
+	config = ConfigParser.RawConfigParser()
 	config.read('config.file')
 
-	#get periods in seconds	from config file
-	NewMajorPeriod = config.getfloat('section 1', 'MajorPeriod')
-	MinorPeriod = config.getfloat('section 1', 'MinorPeriod')
+	#read config file into temporary variables
+	New_LED = config.getint('section 1', 'LED address')
+	New_MajorPeriod = config.getfloat('section 1', 'MajorPeriod')
+	New_MinorPeriod = config.getfloat('section 1', 'MinorPeriod')
+	New_level_name = config.get('section 1', 'LogLevel')
+	New_Log_Filename = config.get('section 1', 'LogFile')
 
-	#set output register on Labjack
-	LED = config.getint('section 1', 'LED address')
+	
+	#test for change in log name and level.  has to be done here to get looging going.
+	old_log_name = ()
+	if Log_Filename != New_Log_Filename:
+		old_log_name = 1 
+		Log_Filename = New_Log_Filename
 
-	level_name = config.get('section 1', 'LogLevel')
-	LOG_FILENAME = config.get('section 1', 'LogFile')
+#this doesn't work correctly because the names are mapped to numeric values.
+#	old_log_level = ()
+	if New_level_name != Log_Level:
+#		old_log_level = 1 
+		Log_Level = New_level_name
 
-	#
-	#flesh out configuration of logging setup and sleep time
-	#
-	Level = LEVELS.get(level_name, logging.NOTSET)
-	logging.basicConfig(filename=LOG_FILENAME,level=Level)
+	#configure logging
+	Log_Level = LEVELS.get(Log_Level, logging.NOTSET)
+	logging.basicConfig(filename=Log_Filename,level=Log_Level)
 
-	#check for change in major period, log and set
-#	logging.debug("NewMajorPeriod = %s and MajorPeriod = %s" % NewMajorPeriod MajorPeriod )
-	if MajorPeriod != NewMajorPeriod:
-		logging.info("config changed for MajorPeriod!")
-#		logging.info("%s: detected config change: MajorPeriod changed from %s to %s" % Start MajorPeriod NewMajorPeriod )
-		MajorPeriod = NewMajorPeriod
+	#log any changes to log name and level
+	if old_log_name:
+ 		logging.info('Logfile set to:')
+#	if old_log_level:
+#		logging.info('Log level set to:')
+
+	#test if other parameters have changed vs new values from file.  if yes then log and update dictionary.
+	if New_LED != LED:
+		logging.info('LED address set to:')
+		LED = New_LED
+	if New_MajorPeriod != MajorPeriod:
+		logging.info('Major Period set to:')
+		MajorPeriod = New_MajorPeriod
+	if New_MinorPeriod != MinorPeriod:
+		logging.info('Minor Period set to:')
+		MinorPeriod = New_MinorPeriod
+
 
 	#calculate time to sleep 
-	NapTime = float(MajorPeriod)/float(MinorPeriod)
+	NapTime = MajorPeriod/MinorPeriod
 
 	# round Target up to nearest even time
 	Overplus = Target % MajorPeriod
@@ -67,7 +90,7 @@ while 1 != 0:
 
 	if Start >= Target:
 
-		logging.debug('This message should go to the log filcheived MajorPeriode')
+		logging.debug('This message should go to the log file')
 
 		Target = Target + MajorPeriod
 
