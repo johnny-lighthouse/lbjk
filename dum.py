f3a9a7733727a67bba2e5966750df08200c850ed
Johnny Lighthouse <git.x.jjl@9ox.net>
2012-10-12 10:06:14 -0400

    move all calls to LabjackPython library to top of dum.py in functions to isolate

--- dum.py
+++ dum.py
@@ -1,12 +1,21 @@
+#dump acceleromter voltage to stdout
 
+#
+# LabjackPython library calls
+#
 
-#LabjackPython library calls
 import u3
 LED = 6004	#address of LED, 6000 register is DIO
 X_Axis = 00	#address of acceleromter, 00 register is AIN
 
 Device = u3.U3(autoOpen = False)
 
+def Open_Device():
+	Device.open()
+
+def Close_Device():
+	Device.close()
+	
 def Read_X():
 	return Device.readRegister(X_Axis)
 
@@ -17,7 +26,9 @@ def Flip_LED():
 	print "LED is %s" % LED_State
 	Device.writeRegister(LED, LED_State)   
 
-
+#
+# other setup
+#
 
 
 import time
@@ -28,6 +39,10 @@ MinorPeriod = .5
 
 #set an initial Target time for first iteration and round up to a whole second
 Target = math.ceil(time.time())
+
+#
+# main loop
+#
 
 while 1 != 0:
 
@@ -45,18 +60,15 @@ while 1 != 0:
 
 		Target = Target + MajorPeriod
 
-		#initialize labjack on each loop
-		Device.open()
+		Open_Device()
 
 		Flip_LED()
 
 		#Read and print value of accelerometer
-#		Accel = Device.readRegister(00)
 		Accel = Read_X()
 		print "Acceleromter output is %s Volts" % Accel
 
-		#close the labjack on eachloop
-		Device.close()
+		Close_Device()
 
 	else:
 		Remainder = Target - Start
