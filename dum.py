6143ee0c11ccc9f8ff314628a8b4f4d9228a4332
Johnny Lighthouse <git.x.jjl@9ox.net>
2012-10-12 23:38:56 -0400

    minor refactoring of element order for clarity

--- dum.py
+++ dum.py
@@ -1,3 +1,4 @@
+#############
 # periodically poll labjack for current acceleromter voltage and print to stdout
 
 #############
@@ -13,23 +14,16 @@ def Open_Device():
 def Close_Device():
 	Device.close()
 
-	
-X_Axis = 00		#address of acceleromter, 00 register is AIN
+X_Axis = 00	#address of acceleromter, 00 register is AIN
+
 def Read_X():
 	return Device.readRegister(X_Axis)
 
+LED = 6004	#address of LED, 6000 register is DIO
 
-LED = 6004		#address of LED, 6000 register is DIO
-LED_State = ()
 def Write_LED():
 	Device.writeRegister(LED, LED_State)   
 
-
-def Flip_LED():
-	global LED_State 
-	LED_State = not LED_State
-	print "LED is %s" % LED_State
-	Write_LED()
 
 ###############
 # other setup
@@ -42,14 +36,14 @@ MinorPeriod = .5
 
 #set an initial Target time for first iteration and round up to a whole second
 Target = math.ceil(time.time())
-Start = ()
-NapTime = ()
-Remainder = ()
 
 def Increment_Target():
 	global Target
 	Target = Target + MajorPeriod
 
+Start = ()
+NapTime = ()
+Remainder = ()
 
 def Set_Timer():
 	global Start
@@ -66,10 +60,16 @@ def Set_Timer():
 	if Overplus != 0:
 		Target = Target + ( MajorPeriod - Overplus )
 
+LED_State = ()
 
-#
+def Flip_LED():
+	global LED_State 
+	LED_State = not LED_State
+	Write_LED()
+
+
+###############
 # main loop
-#
 
 while 1 != 0:
 
@@ -82,6 +82,7 @@ while 1 != 0:
 		Open_Device()
 
 		Flip_LED()
+		print "LED is %s" % LED_State
 
 		#Read and print value of accelerometer
 		Accel = Read_X()
